66752ba4c764d7411bb68f5c309bb3ecd1c49021
Ma5nu <ma5nu.100@gmail.com>
2025-08-17 12:32:26 -0300

    Adiciona Calculadora de Numero Inteiro

---
+++ Calculadora_de_Numero_Inteiro.py
@@ -0,0 +1,13 @@
+print("Digite o primeiro numero: ")
+num1 = int(input())
+print("\nDigite o segundo valor: ")
+num2 = int(input())
+print("\nDigite o terceiro valor: ")
+num3 = int(input())
+print("\n Digite o quarto valor: ")
+num4 = int(input())
+
+#calculo
+diferenca = ((num1 * num2) - (num3 * num4))
+
+print("\n A diferença é: ", diferenca)
