3fee10175c44a93ff1f4facd4e993d3b42856e57
Ma5nu <ma5nu.100@gmail.com>
2025-08-17 12:42:16 -0300

    Adiciona Calculadora de Soma

---
+++ Calculadora_de_Soma.py
@@ -0,0 +1,13 @@
+#Variáveis
+num1 = 0
+num2 = 0
+
+print("Calculadora de Soma\n")
+print("Digite o primeiro numero: ")
+num1 = float(input())
+print("\nDigite o segundo numero: ")
+num2 = float(input())
+
+#Calculo
+total = num1 + num2
+print("\n A Soma é: ", total)
