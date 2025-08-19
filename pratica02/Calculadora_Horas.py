cb8fe9e5b21d93ba5630edeca4092da7d20bee07
Ma5nu <ma5nu.100@gmail.com>
2025-08-19 12:31:01 -0300

    Adicinando nova atividade

---
+++ pratica02/Calculadora_Horas.py
@@ -0,0 +1,5 @@
+print("Calculadora de Horas Trabalhadas")
+horas_trabalhadas = float(input("Digite a quantidade de horas trabalhadas: "))
+valor_por_hora = float(input("Digite o valor pago por hora: "))
+salario = horas_trabalhadas * valor_por_hora
+print(f"O salário total é: R$ {sakario:.2f}")
---
+++ pratica02/Calculadora_Soma_Entrada.py
@@ -0,0 +1,10 @@
+
+print(f"Calculadora de Soma")
+
+num1 = float(input("Digite o primeiro numero: "))
+num2 = float(input("Digite o segundo numero: "))
+
+print(f"O valor total é: {num1 + num2:.2f}")
+
+
+
