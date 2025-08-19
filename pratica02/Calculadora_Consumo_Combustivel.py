e736c7820d5a380b8c242d3d5e4e17c1acf8defa
Ma5nu <ma5nu.100@gmail.com>
2025-08-19 12:17:44 -0300

    Adicionando essa nova atividade

---
+++ pratica02/Calculadora_Consumo_Combustivel.py
@@ -0,0 +1,4 @@
+combustivel = float(input("Digite o valor do combustivel em litros: "))
+distancia = float(input("Digite a distancia que você percorrerá em km: "))
+consumo = distancia / combustivel
+print(f"O consumo de combustivel será de {consumo:.2f} km/l"))
