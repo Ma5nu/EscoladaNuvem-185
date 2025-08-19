8a3bdc95e146b3d8e981b2412287758c694e52fa
Ma5nu <ma5nu.100@gmail.com>
2025-08-19 12:06:57 -0300

    Adicionando nova atividade

---
+++ pratica02/Calculadora_Media_Escolar.py
@@ -0,0 +1,8 @@
+nota1 = float(input("Digite a nota 1:"))
+nota2 = float(input("Digite a nota 2:"))
+nota3 = float(input("Digite a nota 3:"))
+
+media = (nota1 + nota2 + nota3) /3
+
+print(f"A média final é: {media:.2f}")
+print("Fim!")
