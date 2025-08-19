b70a50afa388d4f9aeb3620835683afa57453e90
Ma5nu <ma5nu.100@gmail.com>
2025-08-19 13:54:46 -0300

    Subindo projeto completo

--- pratica02/Calculadora_Horas.py
+++ pratica02/Calculadora_Horas.py
@@ -2,4 +2,4 @@ print("Calculadora de Horas Trabalhadas")
 horas_trabalhadas = float(input("Digite a quantidade de horas trabalhadas: "))
 valor_por_hora = float(input("Digite o valor pago por hora: "))
 salario = horas_trabalhadas * valor_por_hora
-print(f"O salário total é: R$ {sakario:.2f}")
+print(f"O salário total é: R$ {salario:.2f}")
